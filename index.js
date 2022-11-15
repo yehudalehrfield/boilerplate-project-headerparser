605e5468e0c10bb4f491a0c27c19bf41108dbc94
Yehuda Lehrfield <yll2113@columbia.edu>
2022-11-15 15:45:43 -0500

    feat: working version. still issues with ip address

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  console.log(req.ip);
+  res.json({
+    // ipaddress: req.ip,
+    ipaddress: req.headers['x-forwarded-for'] || req.ip,
+    language:req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
